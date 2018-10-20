5d8ad15c185f6ac80c3db220d26cf25a6c223e1e
Brian Little <brilit96@gmail.com>
2018-10-20 00:55:04 -0700

    Verification email now sent immediately upon signing up. User can also request to resend it.
    Password strength added: Must contain a number, uppercase character, and be at least 8 characters long. Feedback is given while the user creates their password.

--- app/src/main/java/com/blittle/sportstatsapp/SignUpActivity.java
+++ app/src/main/java/com/blittle/sportstatsapp/SignUpActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,6 +29,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     private EditText editTextEmail;
     private EditText editTextPassword;
     private TextView textViewSignIn;
+    private TextView textViewViablePassword;
 
     private FirebaseAuth firebaseAuth;
 
@@ -43,10 +47,84 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
         buttonRegister = findViewById(R.id.buttonRegister);
 
-        editTextConfirmPassword = findViewById(R.id.editTextConfirmPassword);
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
+        editTextConfirmPassword = findViewById(R.id.editTextConfirmPassword);
+        textViewViablePassword = findViewById(R.id.textViewViablePassword);
 
+        editTextPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String password = editTextPassword.getText().toString().trim();
+                String confirmPassword = editTextConfirmPassword.getText().toString().trim();
+                textViewViablePassword.setText("");
+
+                if(!password.equals(confirmPassword)) {
+                    textViewViablePassword.setText("Passwords MUST match\n");
+                    return;
+                }
+                if(!password.matches("(.*)[0-9](.*)")) {
+                    textViewViablePassword.append("Password MUST contain a number\n");
+                }
+                if(!password.matches("(.*)[A-Z](.*)")) {
+                    textViewViablePassword.append("Password MUST contain an uppercase character\n");
+                }
+                if(password.length() < 7) {
+                    textViewViablePassword.append("Password MUST be at least 8 characters\n");
+                }
+
+                if(checkPasswordStrength()) {
+                    textViewViablePassword.setText("Password is strong enough");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        editTextConfirmPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String password = editTextPassword.getText().toString().trim();
+                String confirmPassword = editTextConfirmPassword.getText().toString().trim();
+                textViewViablePassword.setText("");
+
+                if(!password.equals(confirmPassword)) {
+                    textViewViablePassword.setText("Passwords MUST match\n");
+                    return;
+                }
+                if(!password.matches("(.*)[0-9](.*)")) {
+                    textViewViablePassword.append("Password MUST contain a number\n");
+                }
+                if(!password.matches("(.*)[A-Z](.*)")) {
+                    textViewViablePassword.append("Password MUST contain an uppercase character\n");
+                }
+                if(password.length() < 7) {
+                    textViewViablePassword.append("Password MUST be at least 8 characters\n");
+                }
+
+                if(checkPasswordStrength()) {
+                    textViewViablePassword.setText("Password is strong enough");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
         textViewSignIn = findViewById(R.id.textViewSignIn);
 
         buttonRegister.setOnClickListener(this);
@@ -71,8 +149,8 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
             //confirm password is empty
             Toast.makeText(this, "Please confirm your password", Toast.LENGTH_SHORT).show();
             return;
-        } else if(!password.equals(confirmPassword)) {
-            Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+        } else if(!checkPasswordStrength()) {
+            Toast.makeText(this, "Passwords do not match, or is not strong enough", Toast.LENGTH_SHORT).show();
             return;
         }
 
@@ -84,9 +162,18 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()) {
                             FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                            //Send verification email
+                            user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Toast.makeText(SignUpActivity.this, "Email verification sent", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                            //Set Display Name for User
                             UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
                                     .setDisplayName("Default").build();
-
                             user.updateProfile(profileUpdates)
                                     .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<Void>() {
                                         @Override
@@ -109,6 +196,21 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
                         }
                     }
                 });
+    }
+
+    private boolean checkPasswordStrength() {
+        String password = editTextPassword.getText().toString().trim();
+        String confirmPassword = editTextConfirmPassword.getText().toString().trim();
+
+        //Check if passwords are the same, contains a number, uppercase character, and is longer than 8 characters.
+        if(password.equals(confirmPassword)) {
+            if(password.matches("(.*)[0-9](.*)") && password.matches("(.*)[A-Z](.*)") && password.length() > 7) {
+                return true;
+            }
+        }
+
+        //If it makes it here, the password was not strong enough.
+        return false;
     }
 
     @Override
